7e4271335280b8881a977dd5cdf85e96431588c5
kiran <kiranday32@gmail.com>
2025-10-25 12:49:37 +0530

    using application properties

--- src/main/java/com/example/pro2/HomeControler.java
+++ src/main/java/com/example/pro2/HomeControler.java
@@ -1,12 +1,16 @@
 package com.example.pro2;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeControler {
+    @Value("${spring.application.name}")
+    private String appName;
     @RequestMapping("/")
     public String Index(){
+        System.out.println("app name:- "+appName );
         return "index.html";
     }
 }
